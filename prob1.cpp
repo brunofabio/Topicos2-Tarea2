951f2fd2f750b70c556d70e25db10991fd118041
brunofabio <bbedonv@uni.pe>
2018-04-14 23:22:35 -0500

    Definicion de variables

---
+++ prob1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<iomanip>
+using namespace std;
+
+const double a = 0.05;
+const double b = 0.002;
+const double c = 0.06;
+const double d = 0.004;
